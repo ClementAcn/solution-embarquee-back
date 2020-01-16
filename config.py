6587da87ae580647fd5cd66463d04a5f38b28eee
Clément Aucoin <c.aucoin@protonmail.com>
2020-01-16 18:04:27 +0100

    Ajout de la secret key

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # Fichier de configuration
 import os
 
+SECRET_KEY = '\x8d\xfd\xcab\xcer\xccE\xdf\xd8\xfe\xf1\xe3\xa9\xd0\xee'
+
+# Initialisation de la base de données
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
