c03a152e8e1cd70d12e2db6ea49038731e2483b5
Clément Aucoin <c.aucoin@protonmail.com>
2020-01-16 15:33:03 +0100

    Test api + set gitignore

---
+++ api.py
@@ -0,0 +1,15 @@
+import flask
+import sqlite3
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/send_data/<int:id>', methods=['GET'])
+def send_data(id):
+  #returns the post, the post_id should be an int
+  co2 = request.args['co2']
+  res = 'Carte n° ' + str(id) + '<br>co2 : ' + co2 + 'ppm'
+  return res
+
+app.run()
