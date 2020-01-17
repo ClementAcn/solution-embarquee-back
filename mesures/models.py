d4fb55dc3588978e98bb1b2729a8a57a998821c7
Clément Aucoin <c.aucoin@protonmail.com>
2020-01-17 12:57:50 +0100

    Ajout colonne capteur

--- mesures/models.py
+++ mesures/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class CaptureCO2(models.Model):
     date = models.DateField(auto_now=True)
     ppm = models.IntegerField(blank=False, null=False, default=0)
+    capteur = models.CharField(max_length=16)
--- mesures/serializers.py
+++ mesures/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class CaptureCO2Serializer(serializers.Serializer):
     date = serializers.DateTimeField()
     ppm = serializers.IntegerField()
+    capteur = serializers.CharField(max_length=16)
