080f6afa95b5c0bb61399e0abb298f0f64d328b4
Clément Aucoin <c.aucoin@protonmail.com>
2020-01-17 13:39:47 +0100

    Add create method CO2

--- mesures/views.py
+++ mesures/views.py
@@ -10,10 +10,13 @@ class CaptureCO2ViewSet(viewsets.ViewSet):
     def list(self, request):
         queryset = CaptureCO2.objects.all()
         serializer = CaptureCO2Serializer(queryset, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
 
     def retrieve(self, request, pk=None):
         queryset = CaptureCO2.objects.all()
         data = get_object_or_404(queryset, pk=pk)
         serializer = CaptureCO2Serializer(data)
-        return Response(serializer.data)
+        return Response(serializer.data, status=200)
+
+    def create(self, request):
+        pass
