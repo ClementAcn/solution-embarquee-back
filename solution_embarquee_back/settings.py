f8b88d450314a41a8000fc9a47e918d903d2db27
Clément Aucoin <c.aucoin@protonmail.com>
2020-01-17 15:11:05 +0100

    Set allowed_hosts

--- solution_embarquee_back/settings.py
+++ solution_embarquee_back/settings.py
@@ -30,7 +30,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['epsi-solution-embarquee-back.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
