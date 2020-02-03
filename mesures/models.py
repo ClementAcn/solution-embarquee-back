66237aecf337cab9a43cc27581c127bf2a5e1335
Clément Aucoin <c.aucoin@protonmail.com>
2020-02-03 14:43:19 +0100

    Set capteur

--- mesures/models.py
+++ mesures/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class CaptureCO2(models.Model):
     date = models.DateField(auto_now=True)
     ppm = models.IntegerField(blank=False, null=False, default=0)
-    capteur = models.CharField(max_length=16)
+    capteur = models.CharField(max_length=16, blank=True, null=True, default="218")
